3163ebbd5eec7a44bce222d7edb963cbd75c5767
esuau <evan.suau@etu.u-pec.fr>
2018-03-07 17:56:03 +0100

    Add authentication component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,18 @@ import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from "@angular/material/toolbar";
 
+import { AppRoutingModule } from './app.routing';
+
 import { AppComponent } from "./app.component";
 import { FeedComponent } from "./components/feed/feed.component";
 import { TweetComponent } from "./components/tweet/tweet.component";
 import { TwitterService } from "./services/twitter.service";
+import { AuthComponent } from './components/auth/auth.component';
 
 @NgModule({
-  declarations: [AppComponent, FeedComponent, TweetComponent],
+  declarations: [AppComponent, FeedComponent, TweetComponent, AuthComponent],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
